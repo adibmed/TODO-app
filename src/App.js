1629e1ec8a470cc9cccc5351c0019a9ad9943161
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-13 16:00:38 +0000

    add todos in UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react'; 
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Take Python course",
+    "Submit Freelance",
+    "Do some sport"
+  ]);
+  const [input, setInput] = useState(''); 
+
+  const addTodo = (event) => {
+    //This will fire off when we click the button 
+    setTodos([...todos, input])
+  } 
+
   return (
     <div className="App">
-      <h1>Hello World🔥! This Adibe here🚀</h1>
-      <input></input>
-      <button>Add Todo</button>
+      <h1>Hello World🔥! This Adibe here🚀 </h1>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
       <ul>
-        <li>Take Python course</li>
-        <li>Submit Freelance</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
 }
 
 export default App;
+ 
